e766ee3cc389262554d4e29453153d6310e3e769
Raphael Salvador <rsalvador1@luc.edu>
2017-05-27 21:47:34 -0300

    greet new user

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ var io = socketIO(server);
 io.on('connection', (socket)=>{
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message)=>{
         console.log('createMessage', message);
@@ -20,6 +30,11 @@ io.on('connection', (socket)=>{
             text: message.text,
             createAt: new Date().getTime()
         })
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect', ()=>{
