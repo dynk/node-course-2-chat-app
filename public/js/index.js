4bf778340e7e1ee755b9303f2c1262fbd9aa2d16
Raphael Salvador <rsalvador1@luc.edu>
2017-05-29 07:50:43 -0300

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 var socket = io();
 
+function scrollToBottom (){
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    
+    messages.scrollTop(scrollHeight);
+    console.log('should scroll')
+  }
+
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,6 +37,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   
   // console.log('newMessage', message);
@@ -37,6 +57,7 @@ socket.on('newLocationMessage', function(message){
   })
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
 
 });
